e787c49539b0158843f780ef266e58ece899f387
Dmitriy Shestakov <altais@ukr.net>
2016-03-23 08:21:05 +0200

    Bootloader in progress

--- main.cpp
+++ main.cpp
@@ -4,30 +4,18 @@
 #include "delay.h"
 #include "timers.h"
 #include "pinlist.h"
-#include "led_driver.h"
+#include "bootloader.h"
 #include <intrinsics.h>
 
 using namespace Mcudrv;
-
-typedef Wk::LedDriver<> LedDriver;
-typedef Wk::ModuleList<Wk::LedDriver<> > moduleList;
-typedef Wk::Wake<moduleList> Wake;
-template void Wake::TxISR();
-template void Wake::RxISR();
+using namespace Wk;
+typedef Bootloader<ID_STM8S003F3> Ldr;
 
 int main()
 {
-//	SysClock::Select(SysClock::HSE);
-	GpioA::WriteConfig<0xFF, GpioBase::In_Pullup>();
- 	GpioB::WriteConfig<0xFF, GpioBase::In_Pullup>();
- 	GpioC::WriteConfig<0xFF, GpioBase::In_Pullup>();
- 	GpioD::WriteConfig<0xFF, GpioBase::In_Pullup>();
-	Wake::Init();
-	enableInterrupts();
-	while(true)
-	{
-		Wake::Process();
-	}
+	Ldr::Init();
+//Never return from here
+	Ldr::Process();
 }
 
 
