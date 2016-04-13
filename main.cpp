436e1b86cc83245634c0ce8c8b798e8ba40ca721
Dmitry Shestakov <altais@ukr.net>
2016-04-13 15:42:17 +0300

    Bootloader in progress

--- main.cpp
+++ main.cpp
@@ -11,17 +11,19 @@ using namespace Mcudrv;
 using namespace Wk;
 typedef Bootloader<ID_STM8S003F3> Ldr;
 
+enum { BOOTSTART = 0x5A };
+
 int main()
 {
   using namespace Uarts;
   Ldr::Init();
   uint8_t count = 0;
-  //wait for request 256 ms and then exit if no request;
+//  wait for request ~256 ms and then exit if no request;
   while(--count) {
-    if(Uart::IsEvent(EvRxne) && Uart::Getch() == FEND) {
+    if(Uart::IsEvent(EvRxne) && Uart::Getch() == BOOTSTART) {
       break;
     }
-    delay_ms(1);
+    delay_us<1000>();
   }
   if(!count) {
     Ldr::Go();
