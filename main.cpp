d16fa6b1911712aab0ec92fef54351f38b6b0b42
Dmitry Shestakov <altais@ukr.net>
2016-04-14 12:38:40 +0300

    Bootloader in progress

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include "stm8s.h"
-#include "itc.h"
-#include "gpio.h"
 #include "delay.h"
-#include "timers.h"
-#include "pinlist.h"
 #include "bootloader.h"
-#include <intrinsics.h>
 
 using namespace Mcudrv;
 using namespace Wk;
 typedef Bootloader<ID_STM8S003F3> Ldr;
-
-enum { BOOTSTART = 0x5A };
 
 int main()
 {
   using namespace Uarts;
   Ldr::Init();
   uint8_t count = 0;
-//  wait for request ~256 ms and then exit if no request;
-  while(--count) {
-    if(Uart::IsEvent(EvRxne) && Uart::Getch() == BOOTSTART) {
-      break;
+  while(true) {
+//wait for request ~256 ms and then (if no request);
+    while(--count) {
+      if(Ldr::ProcessHandshake()) break;
+      delay_us<1000>();
     }
-    delay_us<1000>();
-  }
-  if(!count) {
-    Ldr::Go();
+// try to start user firmware if exist
+    if(!count) Ldr::Go();
+// go to process commands if got the request
+    else break;
+// firmware not exist, new cycle
   }
 //Never return from here
   Ldr::Process();
