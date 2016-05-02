263b62311d1a7a5c29be8acd01c01e9d5e169a8d
Dmitriy Shestakov <altais@ukr.net>
2016-05-02 17:49:42 +0300

    Minor cleanup.

--- main.cpp
+++ main.cpp
@@ -8,20 +8,23 @@ typedef Bootloader<ID_STM8S003F3> Ldr;
 
 int main()
 {
-  using namespace Uarts;
   Ldr::Init();
   uint8_t count = 0;
   while(true) {
 //wait for request ~256 ms and then (if no request);
     while(--count) {
-      if(Ldr::ProcessHandshake()) break;
+			if(Ldr::ProcessHandshake()) {
+				break;
+			}
       delay_us<1000>();
     }
 // try to start user firmware if exist
-    if(!count) Ldr::Go();
+		if(!count) {
+			Ldr::Go();
+		}
 // go to process commands if got the request
     else break;
-// firmware not exist, new cycle
+// firmware not exist, endless wait for a hahdshake
   }
 //Never return from here
   Ldr::Process();
