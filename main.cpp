0540c93e7d21da8bf2831f1e030eaf5c1193e921
Dmitro Shestakov <altais@ukr.net>
2017-07-01 23:23:24 +0300

    Button control implemented. Short tap to On/Off, long tap to change brightness.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #define INSTRUCTION_SET_VER_MAJOR 2
 #define INSTRUCTION_SET_VER_MINOR 1
 
-#define NCP3066
+#define MBI6651
 
 #if defined NCP3066
 #include "led_driver_ncp3066.h"
@@ -21,9 +21,48 @@
 
 using namespace Mcudrv;
 
-typedef Wk::LedDriver<> LedDriver;
-typedef Wk::ModuleList<Wk::LedDriver<> > moduleList;
+//typedef Wk::LedDriver<> LedDriver;
+class LedDriver : public Wk::LedDriver<>
+{
+private:
+  typedef InvertedPin<Pd1> Button;
+  typedef Wk::LedDriver<> Base;
+public:
+  static void Init() {
+    Base::Init();
+    ReleaseSwimPin_Pd1();
+  }
+  static void UpdIRQ() {
+    static uint8_t direction;
+    static uint8_t count;
+    if(Button::IsSet()) {
+      ++count;
+      if(count > 15 && count & 0x01) {
+        if(!direction) {
+          Base::IncBrightness(1);
+        }
+        else {
+          Base::DecBrightness(1);
+        }
+      }
+    }
+    else if(count) {
+      if(count >= 2 && count <= 15) {
+        Base::ToggleOnOff();
+      }
+      else {
+        direction ^= 1;
+      }
+      count = 0;
+    }
+    Base::UpdIRQ();
+  }
+};
+
+typedef Wk::ModuleList<LedDriver> moduleList;
 typedef Wk::Wake<moduleList> Wake;
+
+template void Wake::OpTime::UpdIRQ();
 template void Wake::TxISR();
 template void Wake::RxISR();
 
